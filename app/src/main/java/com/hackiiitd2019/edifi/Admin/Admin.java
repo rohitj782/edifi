e124e3fccf941e7bf1fbb5b1cb5bd7da2e88de0e
Rohit Joshi <rohitj782@gmail.com>
2019-08-23 19:09:27 +0530

    implementing admin

---
+++ app/src/main/java/com/hackiiitd2019/edifi/Admin/Admin.java
@@ -0,0 +1,16 @@
+package com.hackiiitd2019.edifi.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackiiitd2019.edifi.R;
+
+public class Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
--- app/src/main/java/com/hackiiitd2019/edifi/studentorfaculty/StudentOrFaculty.java
+++ app/src/main/java/com/hackiiitd2019/edifi/studentorfaculty/StudentOrFaculty.java
@@ -2,8 +2,12 @@ package com.hackiiitd2019.edifi.studentorfaculty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.hackiiitd2019.edifi.Admin.Admin;
 import com.hackiiitd2019.edifi.R;
 
 public class StudentOrFaculty extends AppCompatActivity {
@@ -12,5 +16,14 @@ public class StudentOrFaculty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_or_faculty);
+
+        TextView textView = findViewById(R.id.textViewAdmin);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StudentOrFaculty.this, Admin.class));
+            }
+        });
+
     }
 }
